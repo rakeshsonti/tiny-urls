b6870a4f6949900f58c52423f17e05e6f3080138
rambhajansonti <23256559@mahindra.com>
2021-11-25 22:01:13 +0530

    Dashboard modifications

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Home from "./components/Home";
 function App() {
   return (
-    <div className="App">
+    <div >
       <Home/>
     </div>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,38 @@
 import '../App.css';
-import { Navbar,Container } from 'react-bootstrap';
+import { Navbar,Container,Button,Form } from 'react-bootstrap';
+import React, { useState } from 'react';
 function Home(){
-    
+const [url, setUrl] = useState("http://google.com");
     return (
         <div>
-<Navbar bg="dark" variant="dark">
-    <Container>
-    <Navbar.Brand href="#">Tiny Urls</Navbar.Brand>
-   
-    </Container>
-  </Navbar>
+            
+            <Navbar bg="dark" variant="dark">
+                <Container>
+                <Navbar.Brand href="#">Tiny Urls</Navbar.Brand>
+                    
+                </Container>
+            </Navbar>
+            <div className="containers">
+            <Form>
+  
 
+  <Form.Group className="mb-3 field1" controlId="formBasicurl">
+    <Form.Label>Enter your URL</Form.Label>
+    <Form.Control type="text" value={url} placeholder="url" onChange={(e)=>{
+        setUrl(e.target.value);
+    }} />
+  </Form.Group>
+  <Button variant="primary" type="button">
+    Submit
+  </Button>
+  <Form.Group className="mb-3 field2" controlId="genurl">
+    <Form.Label>Generated URL</Form.Label>
+    <Form.Control type="text" value={url} placeholder="generated url" />
+    
+  </Form.Group>
+  
+</Form>
+            </div>
         </div>
     )
 }
