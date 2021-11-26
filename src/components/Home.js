e9a920a8da11eebc157a96fc9375b7c31de5ad85
rambhajansonti <23256559@mahindra.com>
2021-11-26 22:00:26 +0530

    spinner added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,14 @@
 import '../App.css';
-import { Navbar,Container,Button,Form ,Alert} from 'react-bootstrap';
+import { Navbar,Container,Button,Form ,Alert,Spinner} from 'react-bootstrap';
 import React, { useState } from 'react';
 function Home(){
 const [url, setUrl] = useState("http://google.com");
 const [genurl, setGenurl] = useState("");
 const [isAlert,setIsAlert]=useState(false);
+const [flag,setFlag]=useState(false);
 const getUrl=(url)=>{
-    
+  setGenurl("");
+  setFlag(true);
     fetch("https://tnny.herokuapp.com/getUrl",{
       method: "POST",
       body: JSON.stringify({
@@ -17,12 +19,15 @@ const getUrl=(url)=>{
       }
     }).then(
       (r) => {
+      setFlag(false);
+
          console.log(r);
          if (r.ok) {
           setGenurl("");
-          alert("success!!!!")
+          // alert("success!!!!")
           return r.json();
          } else {
+           console.log(r);
           alert("Error");
          }
       }
@@ -54,20 +59,33 @@ const copyToClipboard=function(){
   <Form.Group className="mb-3 field1" controlId="formBasicurl">
     <Form.Label>Enter your URL</Form.Label>
     <div className="clear">
-    <Form.Control type="text" value={url} placeholder="url" onChange={(e)=>{
+    <Form.Control type="text" value={url} placeholder="Paste your url" onChange={(e)=>{
         setUrl(e.target.value);
     }} />
+   
     <Button variant="outline-danger" onClick={()=>{
       setUrl("");
       setGenurl("");
     }}>x</Button>
     </div>
   </Form.Group>
-  <Button variant="primary" type="button" onClick={()=>{
+  {
+      flag?<Button variant="dark" disabled>
+      <Spinner
+        as="span"
+        animation="grow"
+        size="sm"
+        role="status"
+        aria-hidden="true"
+      />
+      Genearting...
+    </Button>:<Button variant="primary" type="button" onClick={()=>{
       getUrl(url);
   }}>
     Submit
   </Button>
+    }
+  
   <Form.Group className="mb-3 field2" controlId="genurl">
     <Form.Label>Generated sort URL {" "}</Form.Label>
     {(genurl!=="" && genurl!==null && genurl!==undefined)?<Button type="button" variant="outline-primary" href={genurl} target="_blank"  >{genurl}</Button>:null}
